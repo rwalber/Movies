afe6987d4f30c647c57e059c859fc6930c78b5fc
Walber Rocha <walber_jesus@hotmail.com>
2020-02-05 16:22:57 -0300

    first layout

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="content">
+
+      <div className="movie">
+        <p>Movies</p>
+      </div>
+
+      <div className="inputSearch">
+        <input type="text" placeholder="Busque um filme por nome, ano ou gênero.." />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
